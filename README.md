4070b31f7e67daf3a7bccd9ff1dbe6fa63463db5
Gurjeet Singh <gsgghotra@gmail.com>
2024-05-30 18:20:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vartana
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>Vartana</h2>
+    </>
+  )
+}
+
+export default App
